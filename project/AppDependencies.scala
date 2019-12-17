ab63a3b13cd3cb9776d19c6225a176143436b847
amarnathgujjalapudi <gujjalapudia>
2019-12-17 11:07:13 +0000

    DLS-1393 GMP Bootstrap Upgrade - Frontend(catalogue update)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,18 +8,18 @@ object AppDependencies {
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-25"     % "5.1.0",
     "uk.gov.hmrc" %% "domain"                 % "5.6.0-play-25",
-    "uk.gov.hmrc" %% "http-caching-client"    % "8.4.0-play-25",
-    "uk.gov.hmrc" %% "tax-year"               % "0.5.0",
+    "uk.gov.hmrc" %% "http-caching-client"    % "9.0.0-play-25",
+    "uk.gov.hmrc" %% "tax-year"               % "0.6.0",
     "uk.gov.hmrc" %% "play-partials"          % "6.9.0-play-25",
     "uk.gov.hmrc" %% "emailaddress"           % "3.2.0",
     "uk.gov.hmrc" %% "bulk-entity-streaming"  % "1.0.0",
-    "uk.gov.hmrc" %% "auth-client"            % "2.22.0-play-25",
+    "uk.gov.hmrc" %% "auth-client"            % "2.32.0-play-25",
     "uk.gov.hmrc" %% "govuk-template"         % "5.45.0-play-25",
     "uk.gov.hmrc" %% "play-ui"                % "8.4.0-play-25"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"             %% "hmrctest"           % "3.4.0-play-25",
+    "uk.gov.hmrc"             %% "hmrctest"           % "3.9.0-play-25",
     "org.scalatest"           %% "scalatest"          % "3.0.2",
     "org.scalatestplus.play"  %% "scalatestplus-play" % "2.0.1",
     "org.pegdown"             %  "pegdown"            % "1.6.0",
