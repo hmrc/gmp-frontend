817f4bfd2ace5d9753caf091779dc0df2c0c68e7
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2020-01-22 15:20:05 +0000

    [CU][DLS-1839]: Library upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,13 +9,13 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bootstrap-play-26"     % "1.3.0",
     "uk.gov.hmrc" %% "domain"                 % "5.6.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client"    % "9.0.0-play-26",
-    "uk.gov.hmrc" %% "tax-year"               % "0.6.0",
+    "uk.gov.hmrc" %% "tax-year"               % "1.0.0",
     "uk.gov.hmrc" %% "play-partials"          % "6.9.0-play-26",
-    "uk.gov.hmrc" %% "emailaddress"           % "3.2.0",
+    "uk.gov.hmrc" %% "emailaddress"           % "3.4.0",
     "uk.gov.hmrc" %% "bulk-entity-streaming"  % "1.0.0",
     "uk.gov.hmrc" %% "auth-client"            % "2.32.0-play-26",
-    "uk.gov.hmrc" %% "govuk-template"         % "5.47.0-play-26",
-    "uk.gov.hmrc" %% "play-ui"                % "8.5.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template"         % "5.48.0-play-26",
+    "uk.gov.hmrc" %% "play-ui"                % "8.7.0-play-26",
     "com.typesafe.play" %% "play-json-joda"   % "2.7.4",
     "com.typesafe.play" %% "play-iteratees"   % "2.6.1"
   )
