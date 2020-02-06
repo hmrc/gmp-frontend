828c39d44c847aa40008f45421864154a00a6c07
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2020-02-06 11:30:36 +0000

    [CU][DLS-1877]: SBT Upgrade plus non HMRC Library Upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,7 @@ object AppDependencies {
     "uk.gov.hmrc" %% "bulk-entity-streaming"  % "1.0.0",
     "uk.gov.hmrc" %% "auth-client"            % "2.32.0-play-26",
     "uk.gov.hmrc" %% "govuk-template"         % "5.48.0-play-26",
-    "uk.gov.hmrc" %% "play-ui"                % "8.7.0-play-26",
+    "uk.gov.hmrc" %% "play-ui"                % "8.8.0-play-26",
     "com.typesafe.play" %% "play-json-joda"   % "2.7.4",
     "com.typesafe.play" %% "play-iteratees"   % "2.6.1"
   )
@@ -25,10 +25,10 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"          % "3.0.8",
     "org.scalatestplus.play"  %% "scalatestplus-play" % "3.1.2",
     "org.pegdown"             %  "pegdown"            % "1.6.0",
-    "org.jsoup"               %  "jsoup"              % "1.11.3",
+    "org.jsoup"               %  "jsoup"              % "1.12.1",
     "com.typesafe.play"       %% "play-test"          % PlayVersion.current,
     "org.mockito"             %  "mockito-core"       % "1.9.5",
-    "com.github.tomakehurst"  %  "wiremock-jre8"      % "2.25.1"
+    "com.github.tomakehurst"  %  "wiremock-jre8"      % "2.26.0"
   ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
