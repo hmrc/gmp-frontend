c172b1d424a1a628b1a94d497a058b72c7f05240
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2020-04-08 15:49:01 +0100

    [CU][DLS-2162]: First round of accessibility fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,15 +6,15 @@ object AppDependencies {
   
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26"     % "1.3.0",
+    "uk.gov.hmrc" %% "bootstrap-play-26"     % "1.7.0",
     "uk.gov.hmrc" %% "domain"                 % "5.6.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client"    % "9.0.0-play-26",
     "uk.gov.hmrc" %% "tax-year"               % "1.0.0",
-    "uk.gov.hmrc" %% "play-partials"          % "6.9.0-play-26",
+    "uk.gov.hmrc" %% "play-partials"          % "6.10.0-play-26",
     "uk.gov.hmrc" %% "emailaddress"           % "3.4.0",
     "uk.gov.hmrc" %% "bulk-entity-streaming"  % "1.0.0",
-    "uk.gov.hmrc" %% "auth-client"            % "2.32.0-play-26",
-    "uk.gov.hmrc" %% "govuk-template"         % "5.48.0-play-26",
+    "uk.gov.hmrc" %% "auth-client"            % "2.35.0-play-26",
+    "uk.gov.hmrc" %% "govuk-template"         % "5.54.0-play-26",
     "uk.gov.hmrc" %% "play-ui"                % "8.8.0-play-26",
     "com.typesafe.play" %% "play-json-joda"   % "2.7.4",
     "com.typesafe.play" %% "play-iteratees"   % "2.6.1"
@@ -28,7 +28,7 @@ object AppDependencies {
     "org.jsoup"               %  "jsoup"              % "1.12.1",
     "com.typesafe.play"       %% "play-test"          % PlayVersion.current,
     "org.mockito"             %  "mockito-core"       % "1.9.5",
-    "com.github.tomakehurst"  %  "wiremock-jre8"      % "2.26.0"
+    "com.github.tomakehurst"  %  "wiremock-jre8"      % "2.26.3"
   ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
