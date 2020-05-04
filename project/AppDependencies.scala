522f4171a17dd7994c3997bae390f03fb226e61d
calinursaciuc <31273736+calinursaciuc@users.noreply.github.com>
2020-05-04 15:50:07 +0100

    [CU][DLS-2420]: Library upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,15 +7,15 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc" %% "bootstrap-play-26"     % "1.7.0",
-    "uk.gov.hmrc" %% "domain"                 % "5.6.0-play-26",
+    "uk.gov.hmrc" %% "domain"                 % "5.8.0-play-26",
     "uk.gov.hmrc" %% "http-caching-client"    % "9.0.0-play-26",
     "uk.gov.hmrc" %% "tax-year"               % "1.0.0",
     "uk.gov.hmrc" %% "play-partials"          % "6.10.0-play-26",
     "uk.gov.hmrc" %% "emailaddress"           % "3.4.0",
     "uk.gov.hmrc" %% "bulk-entity-streaming"  % "1.0.0",
-    "uk.gov.hmrc" %% "auth-client"            % "2.35.0-play-26",
+    "uk.gov.hmrc" %% "auth-client"            % "3.0.0-play-26",
     "uk.gov.hmrc" %% "govuk-template"         % "5.54.0-play-26",
-    "uk.gov.hmrc" %% "play-ui"                % "8.8.0-play-26",
+    "uk.gov.hmrc" %% "play-ui"                % "8.9.0-play-26",
     "com.typesafe.play" %% "play-json-joda"   % "2.7.4",
     "com.typesafe.play" %% "play-iteratees"   % "2.6.1"
   )
