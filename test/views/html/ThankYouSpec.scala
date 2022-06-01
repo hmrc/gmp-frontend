1e5e609fe593f6b61b61b5baff718805cb04848e
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2022-06-01 17:40:07 +0100

    DLS-5098: Add hmrcfrontend to routes, update active files to use gmp_main_old to monitor pages being changed, update gmp_main to use hmrc layout

--- test/views/html/ThankYouSpec.scala
+++ test/views/html/ThankYouSpec.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 HM Revenue & Customs
+ * Copyright 2022 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import play.twirl.api.Html
 import utils.GmpViewSpec
 
 class ThankYouSpec extends GmpViewSpec{
-  lazy val gmpMain = app.injector.instanceOf[gmp_main]
+  lazy val gmpMain = app.injector.instanceOf[]
   override def view: Html = new views.html.thank_you(gmpMain)()
 
   "Thank you page" must {
