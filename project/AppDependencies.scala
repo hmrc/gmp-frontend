1174d196d6aa7c41166f1a4cef5e3cb3cd318a70
Ginny Thomas <90277886+ginnyamazed@users.noreply.github.com>
2022-08-04 16:38:45 +0100

    keep app dependencies at stable versions off test apps that do not cause compile errors despite message that more stable versions available

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,7 +6,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28"  % "6.4.0",
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28"  % "5.20.0",
     "uk.gov.hmrc" %% "domain"                      % "8.1.0-play-28",
     "uk.gov.hmrc" %% "http-caching-client"         % "9.6.0-play-28",
     "uk.gov.hmrc" %% "tax-year"                    % "1.1.0",
@@ -15,9 +15,9 @@ object AppDependencies {
     "uk.gov.hmrc" %% "govuk-template"              % "5.77.0-play-28",
     "uk.gov.hmrc" %% "play-ui"                     % "9.6.0-play-28",
     "uk.gov.hmrc" %% "play-frontend-hmrc"          % "3.21.0-play-28",
-    "com.typesafe.play" %% "play-json-joda"        % "2.8.1",
+    "com.typesafe.play" %% "play-json-joda"        % "2.9.2",
     "com.typesafe.play" %% "play-iteratees"        % "2.6.1",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.3",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.13.3",
     compilerPlugin("com.github.ghik" % "silencer-plugin" % "1.7.5" cross CrossVersion.full),
     "com.github.ghik" % "silencer-lib" % "1.7.5" % Provided cross CrossVersion.full
   )
@@ -27,10 +27,10 @@ object AppDependencies {
     "org.scalatestplus.play"  %% "scalatestplus-play" % "5.1.0",
     "org.scalatestplus"      %% "scalatestplus-mockito"   % "1.0.0-M2",
     "org.pegdown"             %  "pegdown"            % "1.6.0",
-    "org.jsoup"               %  "jsoup"              % "1.14.2",
+    "org.jsoup"               %  "jsoup"              % "1.14.3",
     "com.typesafe.play"       %% "play-test"          % PlayVersion.current,
     "org.mockito"             %  "mockito-core"       % "1.10.19",
-    "com.github.tomakehurst"  %  "wiremock-jre8"      % "2.28.0",
+    "com.github.tomakehurst"  %  "wiremock-jre8"      % "2.33.2",
     "com.vladsch.flexmark" % "flexmark-all" % "0.36.8"
   ).map(_ % "test")
 
