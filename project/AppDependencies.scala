46a95bdf3025b9568c4d37386972e1e77fb66fbd
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2022-12-08 09:27:35 +0000

    Correct flexmark version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -30,7 +30,7 @@ object AppDependencies {
     "com.typesafe.play"       %% "play-test"             % PlayVersion.current,
     "org.mockito"             %  "mockito-core"          % "4.9.0",
     "com.github.tomakehurst"  %  "wiremock-jre8"         % "2.35.0",
-    "com.vladsch.flexmark"    %  "flexmark-all"          % "0.36.8"
+    "com.vladsch.flexmark"    %  "flexmark-all"          % "0.62.2"
   ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
