07ddbd725b90c1b7c2dfa75ac3bf6d4520a81ee7
ruthdas03 <86231805+ruthdas03@users.noreply.github.com>
2023-09-28 14:02:59 +0100

    [DLS-6690] WIP on DLS-6690-fix-Inline-error

---
+++ app/forms/helper/Mappings.scala
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.helper
+
+import models.GmpDate
+import play.api.data.FieldMapping
+import play.api.data.Forms.of
+
+import java.time.LocalDate
+
+trait Mappings {
+  protected def gmpLocalDate(
+                              maximumDateInclusive: Option[LocalDate],
+                              minimumDateInclusive: Option[LocalDate],
+                              dayKey: String,
+                              monthKey: String,
+                              yearKey: String,
+                              dateKey: String,
+                              tooRecentArgs: Seq[String] = Seq.empty,
+                              tooFarInPastArgs: Seq[String] = Seq.empty): FieldMapping[GmpDate]=
+
+  of(new GMPLocalDateFormatter(maximumDateInclusive, minimumDateInclusive, dayKey, monthKey, yearKey, dateKey, tooRecentArgs, tooFarInPastArgs))
+}
+
