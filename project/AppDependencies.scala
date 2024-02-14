4372644aa0caae8322c579f7e379054d214d2ce2
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-02-14 14:01:03 +0000

    DLS-9920: Tidies up formatting for AppDependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,25 +8,25 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc"                                %% s"bootstrap-frontend$playSuffix" % "8.4.0",
-    "uk.gov.hmrc"                                %% s"domain$playSuffix"                     % "9.0.0",
-    "uk.gov.hmrc"                                %% s"http-caching-client$playSuffix"        % "11.2.0",
-    "uk.gov.hmrc"                                %% s"play-partials$playSuffix"              % "9.1.0",
-    "uk.gov.hmrc"                                %% s"play-frontend-hmrc$playSuffix"         % bootstrapVersion,
-    "uk.gov.hmrc"                                %% "tax-year"                   % "4.0.0",
-    "uk.gov.hmrc"                                %% "emailaddress"               % "3.8.0",
-    "org.typelevel"                              %% "cats-core"                  % "2.10.0",
-    "com.fasterxml.jackson.module"               %% "jackson-module-scala"       % "2.16.1",
-    compilerPlugin("com.github.ghik" %  "silencer-plugin"            % "1.7.14" cross CrossVersion.full),
-    "com.github.ghik"                            %  "silencer-lib"               % "1.7.14" % Provided cross CrossVersion.full
+    "uk.gov.hmrc"                                %% s"bootstrap-frontend$playSuffix"  % "8.4.0",
+    "uk.gov.hmrc"                                %% s"domain$playSuffix"              % "9.0.0",
+    "uk.gov.hmrc"                                %% s"http-caching-client$playSuffix" % "11.2.0",
+    "uk.gov.hmrc"                                %% s"play-partials$playSuffix"       % "9.1.0",
+    "uk.gov.hmrc"                                %% s"play-frontend-hmrc$playSuffix"  % bootstrapVersion,
+    "uk.gov.hmrc"                                %% "tax-year"                        % "4.0.0",
+    "uk.gov.hmrc"                                %% "emailaddress"                    % "3.8.0",
+    "org.typelevel"                              %% "cats-core"                       % "2.10.0",
+    "com.fasterxml.jackson.module"               %% "jackson-module-scala"            % "2.16.1",
+    compilerPlugin("com.github.ghik" %  "silencer-plugin"                 % "1.7.14" cross CrossVersion.full),
+    "com.github.ghik"                            %  "silencer-lib"                    % "1.7.14" % Provided cross CrossVersion.full
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.mockito" %% "mockito-scala-scalatest" % "1.17.30",
-    "org.scalatestplus" %% "scalacheck-1-17" % "3.2.17.0",
-    "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.0",
-    "uk.gov.hmrc" %% s"bootstrap-test$playSuffix" % bootstrapVersion,
-    "org.jsoup"               %  "jsoup"                 % "1.15.4"
+    "org.mockito"            %% "mockito-scala-scalatest"    % "1.17.30",
+    "org.scalatestplus"      %% "scalacheck-1-17"            % "3.2.17.0",
+    "org.scalatestplus.play" %% "scalatestplus-play"         % "7.0.0",
+    "uk.gov.hmrc"            %% s"bootstrap-test$playSuffix" % bootstrapVersion,
+    "org.jsoup"              %  "jsoup"                      % "1.15.4"
   ).map(_ % "test")
 
   val all: Seq[ModuleID] = compile ++ test
