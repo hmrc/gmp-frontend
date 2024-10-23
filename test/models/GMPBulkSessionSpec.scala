7c0efbebcdb5e4db9c4ede8baa6f3c478a6f828c
emanfalola9998 <77966019+emanfalola9998@users.noreply.github.com>
2024-10-23 15:12:40 +0100

    added spaces and placed license at top of codebase

--- test/models/GMPBulkSessionSpec.scala
+++ test/models/GMPBulkSessionSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package models
 
 import play.api.libs.json.Json
@@ -15,10 +31,10 @@ import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 
 class GMPBulkSessionSpec extends  AnyFreeSpec with Matchers with ScalaFutures with IntegrationPatience with OptionValues
-with GuiceOneAppPerSuite with FutureAwaits with DefaultAwaitTimeout with BeforeAndAfterEach{
+with GuiceOneAppPerSuite with FutureAwaits with DefaultAwaitTimeout with BeforeAndAfterEach {
   implicit val encryption: Encryption = app.injector.instanceOf[Encryption]
 
-
+git add
 
   val id: String = "id"
   val callBackData: UploadStatus = UploadedSuccessfully("testReference", "testFileName", "testUrl")
@@ -58,21 +74,6 @@ with GuiceOneAppPerSuite with FutureAwaits with DefaultAwaitTimeout with BeforeA
 }
 
 
-/*
- * Copyright 2024 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 
 
