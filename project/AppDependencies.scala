2450ca5eb8369d6f9da515fa488255153ff9f8a0
emanfalola9998 <77966019+emanfalola9998@users.noreply.github.com>
2025-11-14 10:36:05 +0000

    DLS-12416: [GMP] Enable service navigation component for gmp-frontend for GOV.UK 2025 Rebrand

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 object AppDependencies {
 
   private val playVersion = "play-30"
-  private val bootstrapVersion = "10.2.0"
+  private val bootstrapVersion = "10.4.0"
   private val hmrcMongoVersion = "2.10.0"
 
   val compile: Seq[ModuleID] = Seq(
@@ -13,7 +13,7 @@ object AppDependencies {
     "uk.gov.hmrc"                                %% s"domain-$playVersion"              % "13.0.0",
     "uk.gov.hmrc"                                %% s"http-caching-client-$playVersion" % "12.2.0",
     "uk.gov.hmrc"                                %% s"play-partials-$playVersion"       % "10.2.0",
-    "uk.gov.hmrc"                                %% s"play-frontend-hmrc-$playVersion"  % "12.17.0",
+    "uk.gov.hmrc"                                %% s"play-frontend-hmrc-$playVersion"  % "12.2.0",
     "uk.gov.hmrc"                                %% "tax-year"                          % "6.0.0",
     "org.typelevel"                              %% "cats-core"                         % "2.12.0",
     "com.fasterxml.jackson.module"               %% "jackson-module-scala"              % "2.18.2",
